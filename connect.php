5b0a8ce3971aca7f6513c1e0c9fc48f99954effe
thereywob <81127870+thereywob@users.noreply.github.com>
2022-11-25 11:52:22 -0500

    Update login info

--- connect.php
+++ connect.php
@@ -2,14 +2,12 @@
 
 $hostname = "localhost";
 $un = "root";
-$pw = "123456";
-$db_name = "csi4480";
+$pw = "Bones123";
+$db_name = "sql_inject_project";
 
 $connect = new mysqli($hostname, $un, $pw, $db_name);
 if ($connect->connect_error){
   die("Connection to database failed: " . $connect->connect_error);
-} else {
-  echo "connection successful";
 }
 
 
