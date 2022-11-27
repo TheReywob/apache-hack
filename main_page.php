4d99dead6503c3f817dfc380144ec7b45628cb53
thereywob <81127870+thereywob@users.noreply.github.com>
2022-11-27 16:15:46 -0500

    Created main page (post-login page)

---
+++ main_page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<body>
+
+<h1>Welcome back, *username*</h1>
+
+<form action="https://google.com">
+    <input type="submit" value="Update Account Information" />
+</form>
+
+</body>
