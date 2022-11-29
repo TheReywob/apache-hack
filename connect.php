efcc2422d16b12c510f484e43da598359414e058
thereywob <81127870+thereywob@users.noreply.github.com>
2022-11-29 14:40:29 -0500

    Hide DB error details

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@ $db_name = "sql_inject_project";
 
 $connect = new mysqli($hostname, $un, $pw, $db_name);
 if ($connect->connect_error){
-  die("Connection to database failed: " . $connect->connect_error);
+  die("Cannot reach the database.");
 }
 
 
